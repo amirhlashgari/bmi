886cc28a52f483f30a31cae3d681ee39d9548556
amirhossein.lashkari <amirhosseinlashgari.al@gmail.com>
2024-03-08 22:34:19 +0330

    add a method to print data of products

--- struct/struct.go
+++ struct/struct.go
@@ -9,6 +9,13 @@ type Product struct {
 	price       float64
 }
 
+func (prod *Product) printData() {
+	fmt.Printf("ID: %v\n", (*prod).id) // no need to destruct Product to use its attributes, (*prod).id === prod.id
+	fmt.Printf("Title: %v\n", prod.title)
+	fmt.Printf("Description: %v\n", prod.description)
+	fmt.Printf("Price: USD %.2f\n\n", prod.price)
+}
+
 func newProduct(id string, name string, desc string, price float64) *Product {
 	return &Product{id, name, desc, price}
 }
@@ -17,6 +24,6 @@ func main() {
 	firstProduct := Product{"123456789", "A Book", "book's description", 6.99} // simple type of defining new product
 	secondProduct := newProduct("987654321", "A Carpet", "nice one", 65.90)    // define product using struct methods
 
-	fmt.Println(firstProduct)
-	fmt.Println(*secondProduct)
+	firstProduct.printData()
+	secondProduct.printData()
 }
