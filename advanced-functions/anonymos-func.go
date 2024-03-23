9a0d3de033f4bdf02a9bc0d3afe51aa98f845f40
amirhossein.lashkari <amirhosseinlashgari.al@gmail.com>
2024-03-23 16:19:41 +0330

    using closures and return a func in another function

--- advanced-functions/anonymos-func.go
+++ advanced-functions/anonymos-func.go
@@ -9,7 +9,15 @@ func main() {
 	// the anonymos function params and return should sync with what defined in transformNumbers2
 	transformed := transformNumbers2(&numbers, func(number int) int { return number * 2 })
 
+	doubledClosure := createTransformerClosure(2)
+	tripledClosure := createTransformerClosure(3)
+
+	transformedDouble := transformNumbers2(&numbers, doubledClosure)
+	transformedTriple := transformNumbers2(&numbers, tripledClosure)
+
 	fmt.Println(transformed)
+	fmt.Println(transformedDouble)
+	fmt.Println(transformedTriple)
 }
 
 func transformNumbers2(numbers *[]int, transform func(int) int) []int {
@@ -20,4 +28,11 @@ func transformNumbers2(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return transformedNumbers
+}
+
+// this function returns another function
+func createTransformerClosure(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
