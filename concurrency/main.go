dbfb5335f068c555e40ab64c6e123b0fe07af659
amirhossein.lashkari <amirhosseinlashgari.al@gmail.com>
2024-03-24 18:55:30 +0330

    IMPORTANT: limiting buffer channels

--- concurrency/main.go
+++ concurrency/main.go
@@ -11,10 +11,12 @@ var randN = rand.New(source)
 
 func main() {
 	c := make(chan int)
+	limiter := make(chan int, 3) // three concurrent tasks in a while(to avoid over performing)
+	// NOTE: channels would be free after sending a value out of a channel
 
 	// x := go generateValue(c) ---> not allowed so we should use channels to return our value
-	go generateValue(c)
-	go generateValue(c)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
 
 	x := <-c
 	y := <-c
@@ -43,11 +45,13 @@ func main() {
 	fmt.Println(sum)
 }
 
-func generateValue(c chan int) int {
+func generateValue(c chan int, limit chan int) int {
+	limit <- 1 // ---> after three times writing 1 to channels it will stop till one ends
 	sleepTime := randN.Intn(3)
 	time.Sleep(time.Duration(sleepTime) * time.Second)
 
 	result := rand.Intn(10)
 	c <- result
+	<-limit       // setting channel free
 	return result // not neccessary
 }
